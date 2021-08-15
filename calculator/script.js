bad7886611a72ccd7cc437a07444c8ac1cdbdef1
abdhulla-k <abdhullak149@gmail.com>
2021-08-15 17:42:20 +0530

    created EqualClic function. it is used 'eval' function for calculation

--- calculator/script.js
+++ calculator/script.js
@@ -5,3 +5,8 @@ function button_click(val){
 function clear_screen(){
     document.getElementById("screen").value=""
 }
+function EqualClic(){                         /*new model equal calculator*/
+   var text = document.getElementById("screen").value
+   var resrlt = eval(text)
+   document.getElementById("screen").value=resrlt
+}
